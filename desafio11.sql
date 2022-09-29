8f29cf7a2d44f35e1455389cce9bb742e2e75d43
Patricia Correia <pncorreia25@gmail.com>
2022-09-29 13:19:03 -0300

    uppercase like

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 SELECT
   cancoes.cancoes_nome AS `nome_musica`,
 CASE
-        WHEN cancoes_nome like '%Bard%' THEN REPLACE(cancoes_nome,'Bard','QA')
-        WHEN cancoes_nome like '%Amar%' THEN REPLACE(cancoes_nome,'Amar','Code Review')
-        WHEN cancoes_nome like '%Pais%' THEN REPLACE(cancoes_nome,'Pais','Pull Requests')
-        WHEN cancoes_nome like '%SOUL%' THEN REPLACE(cancoes_nome,'SOUL','CODE')
-        WHEN cancoes_nome like '%SUPERSTAR%' THEN REPLACE(cancoes_nome,'SUPERSTAR','SUPERDEV')
+        WHEN cancoes_nome LIKE '%Bard%' THEN REPLACE(cancoes_nome,'Bard','QA')
+        WHEN cancoes_nome LIKE '%Amar%' THEN REPLACE(cancoes_nome,'Amar','Code Review')
+        WHEN cancoes_nome LIKE '%Pais%' THEN REPLACE(cancoes_nome,'Pais','Pull Requests')
+        WHEN cancoes_nome LIKE '%SOUL%' THEN REPLACE(cancoes_nome,'SOUL','CODE')
+        WHEN cancoes_nome LIKE '%SUPERSTAR%' THEN REPLACE(cancoes_nome,'SUPERSTAR','SUPERDEV')
         ELSE REPLACE(cancoes_nome,'%', NULL)
     END AS novo_nome
 FROM
